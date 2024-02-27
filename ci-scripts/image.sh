b46cb8b7efa7544265976a9b51c51ecb736fc363
Danny Browning <bdbrowning2@gmail.com>
2024-02-27 14:16:30 -0700

    fix: github and docker just don't play nice

--- ci-scripts/image.sh
+++ ci-scripts/image.sh
@@ -7,4 +7,5 @@ fi
 
 # Build a docker image running checkpointer
 docker buildx build --load -t 3box/checkpointer .
-docker run -e DID_DOCUMENT=$DID_DOCUMENT -e DID_PRIVATE_KEY=$DID_PRIVATE_KEY -e CERAMIC_URL=$CERAMIC_URL --rm 3box/checkpointer ssh-check
+#docker run -e DID_DOCUMENT=$DID_DOCUMENT -e DID_PRIVATE_KEY=$DID_PRIVATE_KEY -e CERAMIC_URL=$CERAMIC_URL --rm 3box/checkpointer ssh-check
+docker run 3box/checkpointer --version
